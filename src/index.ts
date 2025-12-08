2e24d66644a3dfcf92eadbdc9f5d6e2dfaaeec05
Mark Bishoff <markbish24@gmail.com>
2025-12-08 09:54:59 -0800

    thingy

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,42 @@ import express from "express";
 import cors from "cors";
 import { routes } from "./routes.js";
 import "./socket.js";
+import "dotenv/config";
+import { DataSource } from "typeorm";
 
-const app = express();
-
-app.use(express.json());
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:4200",
-      "http://localhost:5000",
-    ],
-  })
-);
-
-routes(app);
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
+const dataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [],
+  logging: false,
+  synchronize: true,
 });
 
-app.listen(8000, () => {
-  console.log("Listening to port 8000");
+dataSource.initialize().then((connection) => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "http://localhost:4200",
+        "http://localhost:5173",
+      ],
+    })
+  );
+
+  routes(app);
+
+  app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+
+  app.listen(8000, () => {
+    console.log("Listening to port 8000");
+  });
 });
--- vue-ws/src/App.vue
+++ vue-ws/src/App.vue
@@ -1,85 +1,89 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ref } from "vue";
+import axios from "axios";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:8080/", {});
+
+const message = ref("");
+const messages = ref([]);
+
+socket.on("message", (msg) => {
+  messages.value = [...messages.value, msg];
+});
+
+const submit = async () => {
+  await axios.post("http://localhost:8000/api/message", {
+    message: message.value,
+  });
+
+  message.value = "";
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div class="container">
+    <div class="row">
+      <div class="col-3 vh-100">
+        <div
+          class="d-flex flex-column align-items-stretch flex-shrink-0 bg-body-tertiary"
+        >
+          <div class="list-group list-group-flush border-bottom scrollarea">
+            <a
+              href="#"
+              class="list-group-item list-group-item-action py-3 lh-sm"
+            >
+              <div
+                class="d-flex w-100 align-items-center justify-content-between"
+              >
+                <strong class="mb-1">Jane Smith</strong>
+                <small class="text-body-secondary">Tues</small>
+              </div>
+              <div class="col-10 mb-1 small">Last Message</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="col-9 border">
+        <div id="head" class="py-3 lh-sm border-bottom">
+          <strong class="mb-1"> John Doe, Jane Smith</strong>
+        </div>
+        <div id="conversation">
+          <div class="row pt-2" v-for="msg of messages">
+            <div class="col-6">
+              <div class="alert alert-primary d-inline-block" role="alert">
+                {{ msg }}
+              </div>
+            </div>
+          </div>
+          <div class="col-6"></div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+          <!-- <div class="row pt-2">
+            <div class="col-6"></div>
+            <div class="col-6">
+              <div
+                class="alert alert-primary d-inline-block float-end"
+                role="alert"
+              >
+                Hello There
+              </div>
+            </div>
+          </div> -->
+        </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+        <form id="reply" class="p-3 w-100" @submit.prevent="submit">
+          <div class="input-group">
+            <input
+              class="form-control"
+              placeholder="write a message"
+              v-model="message"
+            />
+          </div>
+        </form>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
